24cebb09db9df5ca0604ccf05b6723a183eac080
Walison Theodoro <theodoro.walison@gmail.com>
2024-02-18 10:44:44 -0300

    feat: add get all movies method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,9 +9,38 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 
 func main() {
 	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID: "1", Isbn: "438227", Title: "Movie one", Director: &Director{Firstname: "John", Lastname: "Doe"},
+	})
+
+	movies = append(movies, Movie{
+		ID: "2", Isbn: "45455", Title: "Movie two", Director: &Director{Firstname: "Steve", Lastname: "Smith"},
+	})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
 
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
