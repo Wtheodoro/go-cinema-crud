a9013d06dd38129f62989be663ed73cb11707a8b
Walison Theodoro <theodoro.walison@gmail.com>
2024-02-18 10:47:47 -0300

    feat: add delete movie method

--- main.go
+++ main.go
@@ -74,6 +74,20 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if (item.ID == params["id"]) {
+			movies = append(movies[:index], movies[index + 1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -89,6 +103,7 @@ func main() {
 	r.HandleFunc("/movies/{id}", getMovieById).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
